cfddfb1342df784b56c8e7b31251b1dca3463c0e
Miguel Morán Cassy <mmoran@minsait.com>
2020-12-23 14:06:47 +0100

    Parse params

--- apply-lb-patch.sh
+++ apply-lb-patch.sh
@@ -3,11 +3,31 @@
 # if an error occurs the script stops inmediately
 set -e
 
+parseParams() {
+
+  if [[ ${#params[@]} -lt 1 ]]; then
+    echo "Bad params number!"
+    help
+    exit 1
+  fi
+
+  if [[ ${params[0]} != '--module' ]]; then
+    echo "Bad parameter! --module"
+    help
+    exit 1
+  fi
+
+  module=${params[0]}
+
+}
+
 params=("$@")
 
-echo "Using kubectl to apply path to loadbalancer Deployment"
+parseParams
 
-kubectl patch deployment loadbalancer --patch "$(cat patches/'${params[0]'-patch/nginx-config-volumes.yaml)" --namespace $HELM_NAMESPACE
+echo "Using kubectl to apply path to loadbalancer Deployment to module: "$module
+
+kubectl patch deployment loadbalancer --patch "$(cat patches/'$module'-patch/nginx-config-volumes.yaml)" --namespace $HELM_NAMESPACE
 
 echo "loadbalancer Deployment succesfully patched"
 
