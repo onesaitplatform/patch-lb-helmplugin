6ede95e4a0d30b518da6ef34685700272d1bdd4e
Miguel Morán Cassy <mmoran@minsait.com>
2020-12-23 13:02:04 +0100

    Traces added

--- apply-lb-patch.sh
+++ apply-lb-patch.sh
@@ -3,6 +3,10 @@
 # if an error occurs the script stops inmediately
 set -e
 
+echo "Using kubectl to apply path to loadbalancer Deployment"
+
 kubectl patch deployment loadbalancer --patch "$(cat patches/$1-patch/nginx-config-volumes.yaml)" --namespace $HELM_NAMESPACE
+
+echo "loadbalancer Deployment succesfully patched"
 
 $HELM_BIN "${ARGS[@]}"
