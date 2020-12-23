646e703906b882c18e39a15cac1caa19bb368a8a
Miguel Morán Cassy <mmoran@minsait.com>
2020-12-23 10:07:29 +0100

    Point to point test

--- apply-lb-patch.sh
+++ apply-lb-patch.sh
@@ -1,1 +1,8 @@
 # !/bin/bash
+
+# if an error occurs the script stops inmediately
+set -e
+
+kubectl patch deployment loadbalancer --patch "$(cat patches/$1-patch/nginx-config-volumes.yaml)" --namespace $HELM_NAMESPACE
+
+$HELM_BIN "${ARGS[@]}"
