9740b00ffdcb800a7ec8169759a708de2d23d041
Miguel Morán Cassy <mmoran@minsait.com>
2020-12-23 14:51:54 +0100

    Error control

--- apply-lb-patch.sh
+++ apply-lb-patch.sh
@@ -19,6 +19,21 @@ parseParams() {
 
   module=${params[1]}
 
+  returnCode = 0
+  case $module in
+    api|engine|opendata|intelligence|log|things|tools)
+      echo "Right module"
+      returnCode = 0
+      ;;
+    *)
+      echo "Wrong module..."
+      returnCode = 1
+      ;;
+  esac
+
+  if [[ returnCode == 1 ]]; then
+    exit 1
+  fi
 }
 
 params=("$@")
@@ -30,5 +45,19 @@ echo "Using kubectl to apply path to loadbalancer Deployment to module: "$module
 kubectl patch deployment loadbalancer --patch "$(cat $HELM_PLUGIN_DIR/patches/$module-patch/nginx-config-volumes.yaml)" --namespace $HELM_NAMESPACE
 
 echo "loadbalancer Deployment succesfully patched"
+
+# Declare an array and delete arguments
+declare -a ARGS=()
+declare -i argcounter=0
+for var in "$@"; do
+    ((argcounter++))
+
+    # Ignore host and domain arguments
+    if (( $argcounter < 5 )); then
+        continue
+    fi
+
+    ARGS+=($var)
+done
 
 $HELM_BIN "${ARGS[@]}"
