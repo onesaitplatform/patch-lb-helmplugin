fca678dceec1686285d944798cd4768e6901276e
Miguel Morán Cassy <mmoran@minsait.com>
2021-01-20 14:52:50 +0100

    Wait until chart is installed

--- apply-lb-patch.sh
+++ apply-lb-patch.sh
@@ -40,6 +40,22 @@ params=("$@")
 
 parseParams
 
+# Declare an array and delete arguments
+declare -a ARGS=()
+declare -i argcounter=0
+for var in "$@"; do
+    ((argcounter++))
+
+    # Ignore host and domain arguments
+    if (( $argcounter < 3 )); then
+        continue
+    fi
+
+    ARGS+=($var)
+done
+
+$HELM_BIN "${ARGS[@]}"
+
 echo "Using kubectl to apply path to loadbalancer Deployment to module: "$module
 
 echo "Checking if current chart is deployed"
@@ -59,19 +75,3 @@ done
 kubectl patch deployment loadbalancer --patch "$(cat $HELM_PLUGIN_DIR/patches/$module-patch/nginx-config-volumes.yaml)" --namespace $HELM_NAMESPACE
 
 echo "loadbalancer Deployment succesfully patched"
-
-# Declare an array and delete arguments
-declare -a ARGS=()
-declare -i argcounter=0
-for var in "$@"; do
-    ((argcounter++))
-
-    # Ignore host and domain arguments
-    if (( $argcounter < 3 )); then
-        continue
-    fi
-
-    ARGS+=($var)
-done
-
-$HELM_BIN "${ARGS[@]}"
